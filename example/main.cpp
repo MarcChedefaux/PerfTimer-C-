d4a6ccabedbf9e48095ca136d615dac8bc0c5c76
MarcChedefaux <chedefaux.marc@gmail.com>
2024-04-12 21:14:01 +0200

    Creation of an example of utilization

--- example/main.cpp
+++ example/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <thread>
+#include <unistd.h>
 #include "../src/PerfTimer.hpp"
+
+void test_thread()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        auto timer = PerfTimer::start_timer("test");
+        sleep(1);
+        PerfTimer::stop_timer(timer);
+    }
+}
 
 int main()
 {
-    std::cout << "HelloWorld" << std::endl;
+    test_thread();
+    std::cout << PerfTimer::summarize() << std::endl;
 }
